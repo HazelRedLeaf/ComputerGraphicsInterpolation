52c4a35670ed0010f72ba5f0cb62384818cabe36
MM Marinova <mm13354@it052590.users.bris.ac.uk>
2016-02-09 14:15:53 +0000

    Done interpolation

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -34,18 +34,6 @@ int main(int argc, char* argv[])
 
 	SDL_SaveBMP(screen, "screenshot.bmp");
 
-	vector<vec3> result(4);
-	vec3 a(1, 4, 9.2);
-	vec3 b(4, 1, 9.8);
-	Interpolate(a, b, result);
-	for( unsigned int i = 0; i < result.size(); ++i)
-	{
-		cout << "("
-			 << result[i].x << ", "
-			 << result[i].y << ", "
-			 << result[i].z << ") ";
-	}
-	cout << endl;
 	return 0;
 }
 
@@ -63,12 +51,26 @@ void Draw()
 	if(SDL_MUSTLOCK(screen))
 		SDL_LockSurface(screen);
 
+	// colours at the four corners of the screen: 
+	vec3 topLeft(1,0,0);		// red
+	vec3 topRight(0,0,1);		// blue
+	vec3 bottomRight(0,1,0);	// green
+	vec3 bottomLeft(1,1,0);		// yellow
+
+	// interpolate both sides of the screen first
+	vector<vec3> leftSide(SCREEN_HEIGHT);
+	vector<vec3> rightSide(SCREEN_HEIGHT);
+	Interpolate(topLeft, bottomLeft, leftSide);
+	Interpolate(topRight, bottomRight, rightSide);
+
 	for(int y = 0; y < SCREEN_HEIGHT; ++y)
 	{
+		vector<vec3> imageRow(SCREEN_WIDTH);
+		Interpolate(leftSide[y], rightSide[y], imageRow);
 		for(int x = 0; x < SCREEN_WIDTH; ++x)
 		{
 			vec3 color(0.0, 1.0, 0.0);
-			PutPixelSDL(screen, x, y, color);
+			PutPixelSDL(screen, x, y, imageRow[x]);
 		}
 	}
 
